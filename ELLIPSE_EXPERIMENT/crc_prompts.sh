9ab8e423d6228bb725607ae43d181513a5bb0eb9
Alejandro Ciuba <alejandrociuba@gmail.com>
2024-11-13 01:37:46 -0500

    Updated crc_prompts.sh before the ELLIPSE linguistic features experiment

--- ELLIPSE_EXPERIMENT/crc_prompts.sh
+++ ELLIPSE_EXPERIMENT/crc_prompts.sh
@@ -41,6 +41,7 @@ HF_HOME=/ihome/dvillarreal/alc307/ix/ix_models/hugging_face/
 python prompt-llms.py \
     -m mistralai/Mistral-7B-Instruct-v0.2 \
     -d $DIRECTORY$DATA $DIRECTORY$RUBRIC \
-    -l logs/log-noling.log debug/debug-noling.log errors/err-noling.log \
+    -f WORD_COUNT "total number of unique words in the essay" \
+    -l logs/log-ling.log debug/debug-ling.log errors/err-ling.log \
     -t $HF_TOKEN \
     -hf $HF_HOME \
